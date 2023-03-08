35b6f8705cf74c354923fe2c7d715606a6a9f52e
Justin Bak <tkdefender@protonmail.com>
2023-03-08 16:30:11 -0700

    init

---
+++ save_to_card.py
@@ -0,0 +1,15 @@
+# Untitled - By: juicetin - Fri Mar 3 2023
+
+import sensor, image, time
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.VGA)
+sensor.skip_frames(time = 2000)
+
+clock = time.clock()
+
+while(True):
+    clock.tick()
+    img = sensor.snapshot()
+    print(clock.fps())
