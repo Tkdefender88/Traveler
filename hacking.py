0a7a8a1b6d79689af1a3b4d16bda0e5e4f45f8f5
justin <tkdefender@protonmail.com>
2023-04-07 09:58:48 -0600

    expand the protocol class to handle commands correctly

--- hacking.py
+++ hacking.py
@@ -4,27 +4,7 @@ import sensor
 import time
 import ustruct
 import mjpeg
-from pyb import UART
-
-
-class uart_handler():
-
-    def __init__(self):
-        self.uart = None
-
-    def init(self):
-        self.uart = UART(3, 115200, timeout=5000, timeout_char=1000)
-        self.uart.write("start up\n")
-        time.sleep_ms(80)
-
-    def write(self, data):
-        self.uart.write(data)
-
-    def read_line(self):
-        if self.uart.any():
-            return self.uart.readline()
-        else:
-            return None
+from pyb import UART, RTC
 
 
 class Protocol():
@@ -35,21 +15,54 @@ class Protocol():
     STOP = 4
 
     def __init__(self):
-        self.uart = uart_handler()
-        self.uart.init()
-        self.image_compressed = None
+        self.uart = UART(3, 115200, timeout=5000, timeout_char=1000)
+        self.uart.write("start up\n")
+        self.start_byte = 0
+        self.length_to_read = -1
+        self.rtc = RTC()
 
-    def write(self, image, start_byte=0):
-        self.image_compressed = image.compress(quality=80)
-        image_size = len(self.image_compressed)
-        self.uart.write(image)
+        date_time = self.rtc.datetime()
+        print(date_time)
+        self.mjpeg = mjpeg.Mjpeg(
+                "{:04}-{:02}-{:02}-{:02}-{:02}-{:02}.mjpeg".format(
+                    date_time[0],
+                    date_time[1],
+                    date_time[2],
+                    date_time[4],
+                    date_time[5],
+                    date_time[6]
+                )
+            )
 
-        if start_byte > image_size:
-            start_byte = image_size
+    def send_photo(self, image):
+        compressed_image = image.compress(quality=80)
+        image_size = compressed_image.size()
+        print(image_size)
+
+        if self.start_byte > image_size:
+            self.start_byte = image_size
+
+        if self.length_to_read < 0:
+            self.length_to_read = image_size
 
         self.uart.write(bytes([0x76, 0x00, 0x32, 0x00, 0x00, 0xFF, 0xD8]))
-        self.uart.write(image_compressed[start_byte:])
+        self.uart.write(
+            compressed_image.bytearray()[self.start_byte:self.length_to_read]
+        )
         self.uart.write(bytes([0xFF, 0xD9, 0x76, 0x00, 0x32, 0x00, 0x00,]))
+
+    def send_photo_length(self):
+        image_size = sensor.get_fb().compress(quality=80).size()
+        print(image_size)
+        self.uart.write(bytes([0x76, 0x00, 0x34, 0x00, 0x04, 0x00, 0x00]))
+        self.uart.write(
+                bytes(
+                    [(image_size >> 24) & 0xff,
+                     (image_size >> 16) & 0xff,
+                     (image_size >> 8) & 0xff,
+                     (image_size & 0xff)]
+                )
+            )
 
     def command(self):
         cmd = self.uart.read_line()
@@ -68,20 +81,48 @@ class Protocol():
                 ustruct.pack("<bbbb", 0x76, 0x00, 0x26, 0x00)
             )
             return (Protocol.RESET)
+        if cmd[0] == 0x34 and cmd[1] == 0x01 and cmd[2] == 0x00:
+            return self.send_photo_length()
+        if cmd[0] == 0x32 and cmd[1] == 0x0C:
+            return self.command_read_image_data(cmd[2:])
+
         raise OSError("command corrupted")
+
+    def command_read_image_data(self, cmd):
+        if len(cmd) < 11:
+            raise OSError("command corrupted")
+        if cmd[:4] != [0x00, 0x0A, 0x00, 0x00]:
+            raise OSError("command corrupted")
+
+        self.start_byte = cmd[4] << 8 | cmd[5]
+        self.length_to_read = cmd[8] << 8 | cmd[9]
+
+        if self.length_to_read == 0:
+            self.length_to_read = -1
+
+        self.send_photo(sensor.get_fb())
+        return Protocol.READ_DATA_IMAGE
+
+    def capture_image(self):
+        image = sensor.snapshot()
+        self.mjpeg.add_frame(image)
+        image.scale(x_size=640, y_size=480)
 
     def cmd_image(self, cmd):
         if cmd[0] == 0x00:
-            self.uart.send(
-                ustruct.pack("<bbbbb", 0x76, 0x00, 0x36, 0x00, 0x00)
-            )
+            self.uart.write(bytes([0x76, 0x00, 0x36, 0x00, 0x00]))
+            self.capture_image()
             return (Protocol.CAPTURE)
+
         if cmd[0] == 0x03:
-            self.uart.send(
-                ustruct.pack("<bbbbb", 0x76, 0x00, 0x36, 0x00, 0x00)
-            )
+            self.uart.write(bytes([0x76, 0x00, 0x36, 0x00, 0x00]))
+            self.close()
             return (Protocol.STOP)
+
         raise OSError("command corrupted")
+
+    def close(self):
+        self.mjpeg.close(1)
 
 
 def init_board():
@@ -91,18 +132,25 @@ def init_board():
     sensor.skip_frames(time=2000)
 
 
+def testing():
+    try:
+        protocol.capture_image()
+        protocol.send_photo_length()
+        protocol.command_read_image_data(
+            # p      p     p    p      x    x     p     y     y     p      p
+            [0x00, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A]
+        )
+    except OSError as e:
+        print(str(e))
+
+
 init_board()
-clock = time.clock()
 m = mjpeg.Mjpeg("movie.mjpeg")
-
-# 1. UART Read -- wait for signal to take snapshot
-# 2. Take snapshot -- write it to the mjpeg on the SD card
-# 3. Downscale the image
-# 4. write it over serial bus
-
 protocol = Protocol()
 
+testing()
 # Main LOOP WOOOOOO
+'''
 while (True):
     try:
         command = protocol.command()
@@ -115,5 +163,4 @@ while (True):
             break
     except OSError as e:
         print(str(e))
-
-m.close(1)
+'''
