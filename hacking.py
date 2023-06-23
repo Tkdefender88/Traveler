c80e89d7a1dd337a96047d5b9b2cc477e54202f1
Justin Bak <tkdefender@protonmail.com>
2023-06-23 16:49:25 -0600

    added protocol for changing saved image resolution

--- hacking.py
+++ hacking.py
@@ -7,22 +7,33 @@ from pyb import UART
 
 
 class Protocol():
+    VGA = 0
+    QVGA = 1
+    QQVGA = 2
+    IMAGE_COMPRESSION_QUALITY = 50
 
     def __init__(self):
         self.uart = UART(3, 115200, timeout=150, timeout_char=150)
         self.uart.write("start up\n")
+        self.resolution = Protocol.VGA
         self.start_byte = 0
         self.length_to_read = -1
         self.running = True
         self.file_count = 0
+        try:
+            self.mjpeg = mjpeg.Mjpeg("video0.mjpeg")
+        except TypeError as e:
+            print(str(e))
 
-        self.open_new_mjpeg()
+        # self.open_new_mjpeg()
 
     def is_running(self):
         return self.running
 
     def send_photo(self, image):
-        compressed_image = image.compress(quality=50)
+        compressed_image = image.compress(
+            quality=Protocol.IMAGE_COMPRESSION_QUALITY
+        )
         image_size = compressed_image.size()
 
         if self.start_byte > image_size:
@@ -38,8 +49,9 @@ class Protocol():
         self.uart.write(bytes([0xFF, 0xD9, 0x76, 0x00, 0x32, 0x00, 0x00,]))
 
     def send_photo_length(self):
-        image_size = sensor.get_fb().compress(quality=80).size()
-        print("image size " + str(image_size))
+        image_size = sensor.get_fb().compress(
+            quality=Protocol.IMAGE_COMPRESSION_QUALITY
+        ).size()
         self.uart.write(bytes([0x76, 0x00, 0x34, 0x00, 0x04, 0x00, 0x00]))
         self.uart.write(
                 bytes(
@@ -53,7 +65,6 @@ class Protocol():
     def command(self):
         cmd = self.uart.read()
         if cmd is not None:
-            print("Command received")
             print([hex(x) for x in cmd])
             if cmd[0] != 0x56:
                 raise OSError("command corrupted first byte not 0x56")
@@ -68,13 +79,14 @@ class Protocol():
             self.send_photo_length()
         elif cmd[0] == 0x32 and cmd[1] == 0x0C:
             self.command_read_image_data(cmd[2:])
+        elif cmd[0] == 0x31 and cmd[1] == 0x05:
+            self.set_image_resolution(cmd[2:])
         else:
             print([hex(x) for x in cmd])
             raise OSError("command corrupted did not recognize command prefix")
 
     def command_read_image_data(self, cmd):
         if len(cmd) < 9:
-            print(cmd)
             raise OSError(
                 "command corrupted - command length too short for image data"
             )
@@ -86,15 +98,32 @@ class Protocol():
 
         self.send_photo(sensor.get_fb())
 
+    def set_image_resolution(self, cmd):
+        if cmd[0] == 0x00:
+            self.resolution = Protocol.VGA
+        elif cmd[0] == 0x11:
+            self.resolution = Protocol.QVGA
+        elif cmd[0] == 0x22:
+            self.resolution = Protocol.QQVGA
+        else:
+            raise OSError("command for resolution not recognized")
+
     def capture_image(self):
         image = sensor.snapshot()
         self.mjpeg.add_frame(image)
-        image.scale(x_size=640, y_size=480)
+        if self.resolution == Protocol.VGA:
+            image.scale(x_size=640, y_size=480)
+        elif self.resolution == Protocol.QVGA:
+            image.scale(x_size=320, y_size=240)
+        elif self.resolution == Protocol.QQVGA:
+            image.scale(x_size=160, y_size=120)
 
+    '''
     def open_new_mjpeg(self):
-        if self.mjpeg.is_closed():
+        if self.mjpeg is None or self.mjpeg.is_closed():
             self.mjpeg = mjpeg.Mjpeg("video" + str(self.file_count) + ".mjpeg")
             self.file_count += 1
+    '''
 
     def capture_video(self):
         clock = time.clock()
@@ -107,35 +136,28 @@ class Protocol():
             self.mjpeg.add_frame(frame)
             if self.uart.any():
                 line = self.uart.readline()
-                print([hex(x) for x in line])
-                print(line == bytes([0x56, 0x00, 0x5A]))
                 if line == bytes([0x56, 0x00, 0x5A]):
-                    print("respond")
                     self.uart.write(bytes([0x76, 0x00, 0x5A]))
                     watch_dog = 0
                     self.mjpeg.sync(clock.fps())
             else:
                 watch_dog += 1
 
-        print(clock.fps())
         self.mjpeg.close(clock.fps())
 
     def cmd_image(self, cmd):
         if cmd[0] == 0x00:
             self.uart.write(bytes([0x76, 0x00, 0x36, 0x00, 0x00]))
-            print('capture image')
             self.capture_image()
             return
 
         if cmd[0] == 0x01:
             self.uart.write(bytes([0x76, 0x00, 0x36, 0x00, 0x01]))
-            print('capture video')
             self.capture_video()
             return
 
         if cmd[0] == 0x03:
             self.uart.write(bytes([0x76, 0x00, 0x36, 0x00, 0x00]))
-            print('close')
             self.close()
             return
 
@@ -154,6 +176,11 @@ def init_board():
     sensor.skip_frames(time=2000)
 
 
+init_board()
+
+protocol = Protocol()
+
+
 def testing():
     try:
         protocol.capture_image()
@@ -164,19 +191,19 @@ def testing():
         )
         protocol.capture_video()
         protocol.close()
+        print("foo")
     except OSError as e:
+        print(str(e))
+    except TypeError as e:
         print(str(e))
 
 
-init_board()
-
-protocol = Protocol()
-
-# testing()
+testing()
 # Main LOOP WOOOOOO
-
+'''
 while (protocol.is_running()):
     try:
         protocol.command()
     except OSError as e:
         print(str(e))
+'''
